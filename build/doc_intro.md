a2e2a4710b69a33914276d460574c5c7514f5420
Mark Howe <mvahowe@gmail.com>
2020-01-03 17:07:38 +0000

    Documentation

---
+++ build/doc_intro.md
@@ -0,0 +1,1 @@
+This module is a reference implementation of the Scripture Burrito processing model.
--- code/burrito_error.js
+++ code/burrito_error.js
@@ -1,7 +1,9 @@
-/**
-* @type module:BurritoError
-*/
 class BurritoError extends Error {
+    /**
+    A subclass of the standard JS Error class, with support for an optional arg.
+    * @type module:BurritoError
+    * @require Error
+    */
     constructor(msg, arg) {
 	super(msg);
 	if (arg) {
