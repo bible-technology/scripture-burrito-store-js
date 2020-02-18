c044f16ae941df2cdeb2ef0fa7f73031b5d58f82
Mark Howe <mvahowe@gmail.com>
2020-02-18 14:03:30 +0000

    Optional progress section in JSON schema

---
+++ build/prettyprint_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import json, sys
+
+with open(sys.argv[1], 'r') as fp:
+    data = json.load(fp)
+prettified = json.dumps(data, indent=4)
+with open(sys.argv[1], 'w') as fp:
+    fp.write(prettified)
