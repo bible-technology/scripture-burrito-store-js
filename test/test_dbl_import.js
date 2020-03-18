64c3a01635f2ed3d266d83cbaeb702012f384faf
Mark Howe <mvahowe@gmail.com>
2020-03-18 12:05:08 +0000

    Passing DBL import test that does not test anything

--- test/test_dbl_import.js
+++ test/test_dbl_import.js
@@ -5,18 +5,19 @@ const assert = require('chai').assert;
 const path = require('path');
 const fse = require('fs-extra');
 
-const dblImport = require('../code/dbl_metadata_import.js').DBLImport;
+const DBLImport = require('../code/dbl_metadata_import.js').DBLImport;
 
 describe("DBL Import", function() {
 
-    before(function() {
-	this.testDataDir = path.join(__dirname, "test_data");
-	const dblMetadataDir = path.join(this.testDataDir, "dbl_metadata");
-      this.dblTestEntry = fse.readFileSync(path.join(dblMetadataDir, "dbl_test_entry.xml"), "utf8");
-    });
+  before(function() {
+    this.testDataDir = path.join(__dirname, "test_data");
+    const dblMetadataDir = path.join(this.testDataDir, "dbl_metadata");
+    this.dblTestEntry = fse.readFileSync(path.join(dblMetadataDir, "dbl_test_entry.xml"), "utf8");
+    this.convertor = new DBLImport();
+  });
     
     it("Convert DBL Test Entry", function() {
-      dblDomToSBMetadata(this.dblTestEntry);
+      this.convertor.dblDomToSBMetadata(this.dblTestEntry);
     });
 
 });
