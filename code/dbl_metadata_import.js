c72d9dcffe3dcb393194031ae0b0438a85568a75
Mark Howe <mvahowe@gmail.com>
2020-03-18 13:09:02 +0000

    Process root

--- code/dbl_metadata_import.js
+++ code/dbl_metadata_import.js
@@ -1,8 +1,66 @@
 "use strict";
 
+import * as xmldom from 'xmldom';
+import {assert} from 'chai';
+
 class DBLImport {
 
-  dblDomToSBMetadata(dblDom) {}
+  constructor(dblDom) {
+    this.root = dblDom.documentElement;
+    this.sbMetadata = {};
+    this.processRoot();
+    //this.processIdentification();
+    //this.processType();
+    //this.processRelationships();
+    //this.processAgencies();
+    //this.processLanguage();
+    //this.processCountries();
+    //this.processFormat();
+    //this.processNames();
+    //this.processManifest();
+    //this.processSource();
+    //this.processPublications();
+    //this.processCopyright();
+    //this.processPromotion();
+    //this.processProgress();
+    //this.processArchiveStatus();
+  }
+
+  processRoot() {
+    assert.equal(this.root.nodeName, "DBLMetadata");
+    assert.isTrue(this.root.hasAttribute("id"));
+    assert.isTrue(this.root.hasAttribute("revision"));
+    assert.isTrue(parseInt(this.root.getAttribute("version").slice(0,1)) == 2);
+    assert.isTrue(this.root.hasAttribute("revision"));
+    this.sbMetadata = {
+      "meta": {
+        "version": "0.2.0",
+        "variant": "source",
+        "generator": {
+          "softwareName": "DBLImport",
+          "softwareVersion": "0.0.0",
+          "userName": "Mark Howe"
+        },
+        "defaultLanguage": "en"
+      },
+      "idServers": {
+        "dbl": {
+          "id": "https://thedigitalbiblelibrary.org",
+          "name": {
+            "en": "The Digital Bible Library"
+          }
+        }
+      },
+      "identification": {
+        "systemId": {
+          "dbl": {
+            "id": this.root.getAttribute("id"),
+            "revision": this.root.getAttribute("revision")
+          }
+        }
+      }
+    };
+  }
 
 }
 
--- test/test_dbl_import.js
+++ test/test_dbl_import.js
@@ -4,6 +4,7 @@ require = require("esm")(module/*, options*/);
 const assert = require('chai').assert;
 const path = require('path');
 const fse = require('fs-extra');
+const xmldom = require('xmldom');
 
 const DBLImport = require('../code/dbl_metadata_import.js').DBLImport;
 
@@ -12,12 +13,16 @@ describe("DBL Import", function() {
   before(function() {
     this.testDataDir = path.join(__dirname, "test_data");
     const dblMetadataDir = path.join(this.testDataDir, "dbl_metadata");
-    this.dblTestEntry = fse.readFileSync(path.join(dblMetadataDir, "dbl_test_entry.xml"), "utf8");
-    this.convertor = new DBLImport();
+    this.domParser = new xmldom.DOMParser();
+    this.dblTestEntry = this.domParser.parseFromString(
+      fse.readFileSync(path.join(dblMetadataDir, "dbl_test_entry.xml"), "utf8"),
+      'text/xml'
+    );
   });
     
     it("Convert DBL Test Entry", function() {
-      this.convertor.dblDomToSBMetadata(this.dblTestEntry);
+      const converted = new DBLImport(this.dblTestEntry);
+      console.log(JSON.stringify(converted.sbMetadata, null, 2));
     });
 
 });
