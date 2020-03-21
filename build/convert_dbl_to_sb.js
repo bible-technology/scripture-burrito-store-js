9c04d327b080705dcc62a1b32f45cb39160e0213
Mark Howe <mvahowe@gmail.com>
2020-03-21 16:43:19 +0000

    CLI dbl to sb utility

---
+++ build/convert_dbl_to_sb.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require = require("esm")(module/*, options*/);
+const path = require('path');
+const fse = require('fs-extra');
+const xmldom = require('xmldom');
+
+const DBLImport = require('../code/dbl_metadata_import.js').DBLImport;
+const BurritoValidator = require('../code/burrito_validator.js').BurritoValidator;
+
+
+const domParser = new xmldom.DOMParser();
+const entry = domParser.parseFromString(
+  fse.readFileSync(process.argv[2], "utf8"),
+  'text/xml'
+);
+const converted = new DBLImport(entry);
+// console.log(JSON.stringify(converted.sbMetadata.meta, null, 2));
+const validationResult = new BurritoValidator().schemaValidate("metadata", converted.sbMetadata);
+if (validationResult.result == "accepted") {
+  console.log(JSON.stringify(converted.sbMetadata, null, 2));
+} else {
+  console.log(validationResult.result)
+}
