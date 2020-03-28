16a25d78b71a92beb693807a1de5759046ffb03d
Mark Howe <mvahowe@gmail.com>
2020-03-28 09:11:55 +0000

    Rework unit tests

--- test/test_schema.js
+++ test/test_schema.js
@@ -10,16 +10,26 @@ const FSBurritoStore = require("../fs_burrito_store.js").FSBurritoStore;
 describe("Schema", function() {
     before(function() {
         this.testDataDir = path.join(__dirname, "test_data");
+        this.storagePath = path.join(__dirname, "test_temp_storage");
         const metadataDir = path.join(this.testDataDir, "metadata");
         this.validTextTranslation = JSON.parse(
-            fse.readFileSync(path.join(metadataDir, "TextTranslation.json"), "utf8")
+            fse.readFileSync(path.join(metadataDir, "textTranslation.json"), "utf8")
         );
     });
 
+    afterEach(function() {
+        if (fse.existsSync(this.storagePath)) {
+            fse.removeSync(this.storagePath);
+        }
+    });
+    
     it("Accept test textTranslation document", function() {
-        const b = new FSBurritoStore({
-            storeClass: "FSBurritoStore"
-        });
+        const b = new FSBurritoStore(
+            {
+                storeClass: "FSBurritoStore"
+            },
+            this.storagePath
+        );
         try {
             b.importFromObject(this.validTextTranslation);
         } catch (err) {
--- test/test_smoke.js
+++ test/test_smoke.js
@@ -1,3 +1,4 @@
 describe("Schema Tests", function() {
     require("./test_schema.js");
+    require("./test_fs_burrito_store.js");
 });
